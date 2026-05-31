59806096995fb50f2c408682b4761a2f540fc74d
Victor Magos <victormagos@pm.me>
2026-05-30 22:26:49 -0600

    feat: configure DI, EF Core and default route in Program.cs

--- AddressCatalog/Program.cs
+++ AddressCatalog/Program.cs
@@ -1,6 +1,15 @@
+using AddressCatalog.Data;
+using AddressCatalog.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IAddressService, AddressService>();
 
 var app = builder.Build();
 
@@ -11,13 +20,10 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
 app.UseRouting();
-
 app.UseAuthorization();
-
-app.MapStaticAssets();
-app.MapRazorPages()
-   .WithStaticAssets();
+app.MapRazorPages();
+app.MapGet("/", () => Results.Redirect("/Addresses"));
 
 app.Run();
